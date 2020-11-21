b7c6330cc9c03f4c92175df98a10297e5441a1ea
Gracedata <38814297+graceliudata@users.noreply.github.com>
2020-11-21 15:59:07 -0500

    Update autoSim.R

--- resources/speed_up_in_r_programming/autoSim.R
+++ resources/speed_up_in_r_programming/autoSim.R
@@ -13,7 +13,11 @@ for (dist in distTypes) {
     oFile <- paste("n", n,"dist",dist, ".txt", sep="")
     sysCall <- paste("nohup Rscript runSim.R n=", n, " dist=",shQuote(shQuote(dist)),
                      " seed=",seed," rep=",rep," > ", oFile, sep="")
-    system(sysCall)
-    print(paste("sysCall=", sysCall, sep=""))
+    #For we are not allowed to write files into the community contribution repository, 
+    #we commended the following 2 lines that print results in .txt files 
+    #and write those files into current folder. If you would like to try parallel 
+    #computing in this way, feel free to cancel the comments of those 2 lines below.
+    #system(sysCall)
+    #print(paste("sysCall=", sysCall, sep=""))
   }
 }
